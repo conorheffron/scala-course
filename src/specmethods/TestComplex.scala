26f6f4e01af2e295cd1a69163576a04945b7c1c0
Conor Heffron <conor.heffron@gmail.com>
2017-02-21 10:27:11 -0500

    Updated scala jars to version 2.11 and uncommented TestComplex code that will now compile and run

--- src/specmethods/TestComplex.scala
+++ src/specmethods/TestComplex.scala
@@ -1,14 +1,13 @@
-//package specmethods
-//
-//import org.scalatest.FlatSpec
-//
-//
-//class TestComplex extends FlatSpec {
-//  behavior of "Complex numbers"
-//  
-//  it should "add properly" in {
-//    val a = Complex(1,2)
-//    val b = Complex(2,3)
-//    assert(a+b === Complex(3,5))
-//  }
-//}
+package specmethods
+
+import org.scalatest.FlatSpec
+
+class TestComplex extends FlatSpec {
+  behavior of "Complex numbers"
+  
+  it should "add properly" in {
+    val a = Complex(1,2)
+    val b = Complex(2,3)
+    assert(a+b === Complex(3,5))
+  }
+}
